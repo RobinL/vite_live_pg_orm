1084d8ffc7803b8cc913950eb203729e1fa95b3b
Robin Linacre <robinlinacre@hotmail.com>
2025-08-17 10:59:23 +0100

    fix: remove special character from greeting in App component

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@
 export default function App() {
   return (
     <div className="h-screen flex items-center justify-center text-2xl">
-      Hello • Stage 1
+      Hello Stage 1
     </div>
   );
 }
