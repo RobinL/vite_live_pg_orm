73303b18f593ce9993517d5ec5758cec91866407
Robin Linacre <robinlinacre@hotmail.com>
2025-08-18 09:03:56 +0100

    feat: remove unused DDL parsing scripts for foreign key and primary key constraints

--- scripts/ast_alter_pk.mjs
+++
@@ -1,7 +0,0 @@
-import { parse } from 'pgsql-ast-parser';
-
-const ddl = `ALTER TABLE ONLY public.customers
-    ADD CONSTRAINT pk_customers PRIMARY KEY (customer_id);`;
-
-const ast = parse(ddl);
-console.log(JSON.stringify(ast, null, 2));
